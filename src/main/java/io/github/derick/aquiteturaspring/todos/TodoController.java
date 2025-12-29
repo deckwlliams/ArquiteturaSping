b89865193d2f5ffd4478c25f7cafdfc54bc7a53e
DERICK WILLIAMS <deckprogramador@gmail.com>
2025-12-29 19:30:35 -0300

    commit

---
+++ src/main/java/io/github/derick/aquiteturaspring/todos/TodoController.java
@@ -0,0 +1,27 @@
+package io.github.derick.aquiteturaspring.todos;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/todo")
+public class TodoController {
+
+    private TodoService service;
+
+    public TodoController(TodoService service) {
+        this.service = service;
+    }
+    public TodoEntity salvar(@RequestBody TodoEntity todo) {
+       return this.service.salvar(todo);
+
+    }
+    @PutMapping("{id}")
+    public void atualizarStatus(@PathVariable("id") Integer id, @RequestBody TodoEntity todo){
+        todo.setId(id);
+        service.atualizarStatus(todo);
+    }
+    @GetMapping("{id}")
+    public TodoEntity buscar(@PathVariable("id")  Integer id){
+        return service.buscarPorId(id);
+    }
+}
---
+++ src/main/java/io/github/derick/aquiteturaspring/todos/TodoService.java
@@ -0,0 +1,22 @@
+package io.github.derick.aquiteturaspring.todos;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoService {
+
+    public TodoRepository repository;
+    public TodoService(TodoRepository todoRepository){
+        this.repository = todoRepository;
+    }
+    public TodoEntity salvar(TodoEntity novoTodo){
+return repository.save(novoTodo);
+    }
+
+    public void atualizarStatus(TodoEntity todo) {
+        repository.save(todo);
+    }
+    public TodoEntity buscarPorId(Integer id){
+         return repository.findById(id).orElse(null);
+    }
+}
